40cd8f78b7f9c1fb00b2f0699cbca724904a2036
Alyssa Yates <bosslys@icloud.com>
2019-09-25 22:09:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,10 @@
 class Person
 
-  attr_accessor :name, :eye_color, :hair_type, :hair_color
+  attr_accessor :name, :eye_color, :hair_color
 
-  def initialize(name:, eye_color:, hair_type:, hair_color:)
+  def initialize(name:, eye_color:, hair_color:)
     @name = name
     @eye_color = eye_color
-    @hair_type = hair_type
     @hair_color = hair_color
   end
 end

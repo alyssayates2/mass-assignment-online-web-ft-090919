34d3577c16ecbd722ce28a77623eae619abaeb21
Alyssa Yates <bosslys@icloud.com>
2019-09-25 22:10:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,18 @@
 class Person
 
-  attr_accessor :name, :eye_color, :hair_color
+  attr_accessor :name, :eye_color, :hair_type, :hair_color
 
-  def initialize(name:, eye_color:, hair_color:)
+  def initialize(name:, eye_color:, hair_type:, hair_color:)
     @name = name
     @eye_color = eye_color
+    @hair_type = hair_type
     @hair_color = hair_color
   end
-end
+
+
+
+kate_attributes = {name: "Kate", eye_color: "hazel", hair_type: "thick and curly" }
+
+kate = Person.new(kate_attributes)
+
+end 
